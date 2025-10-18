18b518a10b573a2c42c8592cbc89f8b4ab0b1917
HMQui <huynhqui6425@gmail.com>
2025-10-18 14:11:18 +0700

    feat: upload image

--- routes/indexRoute.js
+++ routes/indexRoute.js
@@ -2,6 +2,7 @@ import express from 'express';
 const router = express.Router();
 import authMiddleware from "../middlewares/authMiddleware.js"
 import * as indexController from "../controllers/indexController.js"
+import upload from "../config/multer.js"
 
 /* GET home page. */
 router.get('/', (req, res, next) => {
@@ -10,5 +11,13 @@ router.get('/', (req, res, next) => {
 
 //
 router.get('/support', authMiddleware, indexController.getSupport);
+
+router.get('/upload-image', (req, res) => {
+  return res.render('uploadImage');
+})
+
+router.post('/upload-image', upload.single('image'), (req, res) => {
+  return res.json({ success: 'completed' })
+})
 
 export default router;
